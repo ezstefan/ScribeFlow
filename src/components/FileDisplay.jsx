27d8a3b1f3f7838980dd7c960aa4550bcda3b017
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-08-22 20:00:03 +0530

    modified file display

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function FileDisplay(props){
     const {handleAudioReset, file, audioStream, handleFormSubmission } = props
 
+    
     return(
         <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4
         justify-center pb-20 w-72 sm:w-96 max-w-full mx-auto'>
